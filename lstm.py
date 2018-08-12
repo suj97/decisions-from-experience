af876c9a2c6311291f531d046c40a5f3960c7c26
sujay <sujay.khandagale@gmail.com>
2018-08-12 11:52:39 +0530

    improve LSTM accuracy

--- lstm.py
+++ lstm.py
@@ -3,25 +3,33 @@ import pandas as pd
 from sklearn.cross_validation import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense, Input, Dropout, LSTM, Embedding
+from keras import optimizers
 
 dataset = pd.read_csv('data/ndata.csv')
 
 X = dataset.iloc[:, 0:-1].values
 y = dataset.iloc[:, -1].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+X_train, y_train = X, y
 
 input_length = len(X_train[0])
 
+X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
+# X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
+
+
 # create the model
 model = Sequential()
-model.add(Embedding(input_dim = 3, output_dim = 1, input_length = input_length))
-model.add(LSTM(20))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+# model.add(Embedding(input_dim = 3, output_dim = 2, input_length = input_length))
+model.add(LSTM(100, input_shape=(1, input_length)))
+model.add(Dense(1))
+
+opt = optimizers.Adam()
+model.compile(loss='mae', optimizer=opt, metrics=['accuracy'])
 print(model.summary())
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=20, batch_size=20)
+model.fit(X_train, y_train, epochs=300, batch_size=10)
 
 # Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
+scores = model.evaluate(X_train, y_train, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
