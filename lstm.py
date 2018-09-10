49848236a5ce58764680a25020258a70224d4e0f
Sujay Khandagale <sujay.khandagale@gmail.com>
2018-09-10 18:53:59 +0530

    Add modified test dataset

--- lstm.py
+++ lstm.py
@@ -5,18 +5,21 @@ from keras.models import Sequential
 from keras.layers import Dense, Input, Dropout, LSTM, Embedding
 from keras import optimizers
 
-dataset = pd.read_csv('data/ndata.csv')
+dataset = pd.read_csv('data/estimation.csv')
+dataset_comp = pd.read_csv('data/competition.csv')
 
-X = dataset.iloc[:, 0:-1].values
-y = dataset.iloc[:, -1].values
+X_train = dataset.iloc[:, 0:-1].values
+y_train = dataset.iloc[:, -1].values
+X_test = dataset_comp.iloc[:, 0:-1].values
+y_test = dataset_comp.iloc[:, -1].values
 
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
-X_train, y_train = X, y
+# X_train, y_train = X, y
 
 input_length = len(X_train[0])
 
 X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
-# X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
+X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
 
 
 # create the model
@@ -28,8 +31,8 @@ model.add(Dense(1))
 opt = optimizers.Adam()
 model.compile(loss='mae', optimizer=opt, metrics=['accuracy'])
 print(model.summary())
-model.fit(X_train, y_train, epochs=300, batch_size=10)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, batch_size=20)
 
 # Final evaluation of the model
-scores = model.evaluate(X_train, y_train, verbose=0)
+scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
