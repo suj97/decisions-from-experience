f48f83069c785149cb48a6e7ad5e199cbb8eee58
Sujay Khandagale <sujay.khandagale@gmail.com>
2018-11-24 00:11:54 +0530

    update grid search parameters

--- grid-search.py
+++ grid-search.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense, Input, Dropout, LSTM, GRU, SimpleRNN, Embedding, Conv1D, MaxPooling1D, Masking
 from keras import optimizers,regularizers
